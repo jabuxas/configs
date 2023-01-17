f88c0531965c4855de35a8bd2af2d6c2d6152537
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2023-01-17 18:05:48 -0300

    Add cr mapping in n mode and discord rich presence integration

--- configs/nvim/after/plugin/treesitter.lua
+++ configs/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all"
+    -- a list of parser names, or "all"
     ensure_installed = {
         "help",
         "python",
@@ -15,24 +15,34 @@ require 'nvim-treesitter.configs'.setup {
         "css",
     },
 
-    -- Install parsers synchronously (only applied to `ensure_installed`)
+    -- install parsers synchronously (only applied to `ensure_installed`)
     sync_install = true,
 
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    -- automatically install missing parsers when entering buffer
+    -- recommendation: set to false if you don't have `tree-sitter` cli installed locally
     auto_install = true,
 
     highlight = {
         -- `false` will disable the whole extension
         enable = true,
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
+        -- setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- using this option may slow down your editor, and you may see some duplicate highlights.
+        -- instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
     },
 
     autotag = {
         enable = true,
+    },
+
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<cr>",
+            node_incremental = "<cr>",
+            scope_incremental = "<s-cr>",
+            node_decremental = "<bs>",
+        },
     },
 }
