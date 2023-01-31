cf9c475d210ecf4b3e1d6b57a211860e07f659df
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2023-01-31 14:36:48 -0300

    uhh update lots of stuff and xmobar

--- configs/xmonad/scripts/volumeicon.sh
+++ configs/xmonad/scripts/volumeicon.sh
@@ -4,9 +4,9 @@ MUTE=${MUTE:-$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')}
 VOLUME=${VOLUME:-$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed "s/%//")}
 
 if [[ "${MUTE}" = "yes" ]] ; then 
-    echo "<fc=#6B7089,#31353F:5></fc>"
+    echo ""
 elif [[ "${MUTE}" = "no" ]] && [[ "${VOLUME}" -eq 0 ]] ; then
-    echo "<fc=#6B7089,#31353F:5>${VOLUME}%</fc>"
+    echo " "
 elif [[ "${MUTE}" = "no" ]] && [[ "${VOLUME}" -gt 0 ]] ; then
-    echo "${VOLUME}%"
+    echo " "
 fi
