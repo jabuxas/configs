9ef4f87d6dcef6ef06eb16b2ed3f8c4db443b0b1
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2023-02-27 20:19:42 -0300

    Trim down xmonad config and properly format xmobar

--- configs/xmonad/src/xmobar.hs
+++ configs/xmonad/src/xmobar.hs
@@ -17,33 +17,37 @@ myConfig =
       baseConfig
         { template =
             concat $
-              [ "<fn=2><fc=#292522,#292522:7>\xe0b6</fc></fn>\
-                \<fn=6><fc=#E49B5D,#292522:5>\xf30d </fc></fn>\
-                \<fn=2><fc=#292522,#292522:7>\xe0b4</fc></fn>"
+              [ " <fn=2><fc=#002b36,#002b36:7>\xe0b6</fc></fn>\
+                \<fn=4><fc=#558c8e,#002b36:5>\xf30d </fc></fn>\
+                \<fn=2><fc=#002b36,#002b36:7>\xe0b4</fc></fn> "
               ]
-                <> ["<fn=2>@UnsafeXMonadLog@</fn>}"]
-                <> [ "<fn=2><fc=#292522,#292522:7>\xe0b6</fc></fn>\
-                     \<fn=4><fc=#ebc06d,#292522:5>\xf017 </fc></fn>\
-                     \<fn=5>@date@</fn>{"
+                <> ["<fn=5>@UnsafeXMonadLog@</fn>}"]
+                   <> [ "<fn=2><fc=#002b36,#002b36:7>\xe0b6</fc></fn>\
+                     \<fn=4><fc=#E06C75,#002b36:5>\xf001 </fc></fn>\
+                     \<fn=5><fc=#E06C75,#002b36:5>@music@</fc></fn>\
+                     \<fn=2><fc=#002b36,#002b36:7>\xe0b4</fc></fn>{"
                    ]
-                <> [ "<fn=2><fc=#292522,#292522:7>\xe0b6</fc></fn>\
-                     \<fn=4><fc=#89b3b6,#292522:5>\xf2db </fc></fn>\
-                     \<fn=5>CPU: @cpu@% |</fn>"
+                <> [ "<fn=2><fc=#002b36,#002b36:7>\xe0b6</fc></fn>\
+                     \<fn=4><fc=#56B6C2,#002b36:5>\xf2db </fc></fn>\
+                     \<fn=5><fc=#56B6C2,#002b36:5>CPU: @cpu@%</fc></fn>\
+                     \<fn=2><fc=#002b36,#002b36:7>\xe0b4</fc></fn> "
                    ]
-                <> [ "<fn=2><fc=#292522,#292522:7>\xe0b6</fc></fn>\
-                     \<fn=4><fc=#b380b0,#292522:5>\xf538 </fc></fn>\
-                     \<fn=5>Mem: @memory@% |</fn>"
+                <> [ "<fn=2><fc=#002b36,#002b36:7>\xe0b6</fc></fn>\
+                     \<fn=4><fc=#C678DD,#002b36:5>\xf538 </fc></fn>\
+                     \<fn=5><fc=#C678DD,#002b36:5>Mem: @memory@% </fc></fn>\
+                     \<fn=2><fc=#002b36,#002b36:7>\xe0b4</fc></fn> "
                    ]
-                <> [ "<fn=2><fc=#292522,#292522:7>\xe0b6</fc></fn>\
-                     \<fn=4><fc=#78997a,#292522:5>@volicon@</fc></fn>\
-                     \<fn=5>@vol@<fc=#78997a,#292522:5>%</fc> |</fn>"
+                <> [ "<fn=2><fc=#002b36,#002b36:7>\xe0b6</fc></fn>\
+                     \<fn=4><fc=#98C379,#002b36:5>@volicon@</fc></fn>\
+                     \<fn=5><fc=#98C379,#002b36:5>@vol@</fc></fn>\
+                     \<fn=2><fc=#002b36,#002b36:7>\xe0b4</fc></fn> "
                    ]
-                <> [ "<fn=2><fc=#292522,#292522:7>\xe0b6</fc></fn>\
-                     \<fn=4><fc=#d47766,#292522:5>\xf001 </fc></fn>\
-                     \<fn=5><fc=#d47766,#292522:5>@music@</fc></fn>\
-                     \<fn=2><fc=#292522,#292522:7>\xe0b4</fc></fn>"
+                <> [ "<fn=2><fc=#002b36,#002b36:7>\xe0b6</fc></fn>\
+                     \<fn=4><fc=#61AFEF,#002b36:5>\xf017 </fc></fn>\
+                     \<fn=5><fc=#61AFEF,#002b36:5>@date@</fc></fn>\
+                     \<fn=2><fc=#002b36,#002b36:7>\xe0b4</fc></fn> "
                    ]
-                <> ["| @_XMONAD_TRAYPAD@"],
+                <> ["@_XMONAD_TRAYPAD@"],
           commands = myCommands
         }
 
@@ -52,9 +56,9 @@ myCommands =
   [ Run UnsafeXMonadLog,
     Run $ Com (myHomeDir <> "/.config/xmonad/scripts/volume.sh") ["vol"] "vol" 100,
     Run $ Com (myHomeDir <> "/.config/xmonad/scripts/volumeicon.sh") ["volicon"] "volicon" 100,
-    Run $ Date "%A, %d/%m/%Y. <fc=#ebc06d,#292522:5>%H:%M</fc>" "date" 600,
-    Run $ Cpu ["-t", "<fc=#8c7f80><total></fc>", "-f", ":", "-H", "75", "-L", "25", "-h", "#89b3b6", "-n", "#4797a1", "-l", "#3a7b83"] 100,
-    Run $ Memory ["-t", "<fc=#8c7f80><usedratio></fc>", "-f", ":", "-H", "75", "-L", "25", "-h", "#b380b0", "-n", "#9f60b1", "-l", "#855094"] 100,
+    Run $ Date "%-l:%M %p" "date" 600,
+    Run $ Cpu ["-t", "<fc=#8c7f80><total></fc>", "-f", ":", "-H", "75", "-L", "25", "-h", "#56B6C2", "-n", "#4797a1", "-l", "#3a7b83"] 100,
+    Run $ Memory ["-t", "<fc=#8c7f80><usedratio></fc>", "-f", ":", "-H", "75", "-L", "25", "-h", "#c678dd", "-n", "#9f60b1", "-l", "#855094"] 100,
     -- , Run $ Com (myHomeDir <> "/.config/xmonad/scripts/gputemp.sh") ["gpu"] "gpu" 5
     Run $ XPropertyLog "_XMONAD_TRAYPAD",
     Run $ Com (myHomeDir <> "/.config/xmonad/scripts/mpd.sh") ["music"] "music" 100
@@ -65,27 +69,26 @@ myCommands =
 baseConfig :: Config
 baseConfig =
   defaultConfig
-    { font = "Cartograph CF Italic 11",
+    { font = "Sugar Snow 13",
       additionalFonts =
-        [ "Cartograph CF Italic 10",
-          "Cartograph CF Italic 11",
-          "Cartograph CF Italic 11",
-          "JetBrainsMono Nerd Font 10",
-          "Cartograph CF Italic 11",
-          "JetBrainsMono Nerd Font 13"
+        [ "Sugar Snow 11",
+          "Sugar Snow 13",
+          "Sugar Snow 12",
+          "JetBrainsMono Nerd Font 12",
+          "Sugar Snow 13"
         ],
       -- textOffsets = [20, 22, 21, 21, 20],
-      bgColor = "#292522",
-      fgColor = "#ECE1d7",
+      bgColor = "#002b36",
+      fgColor = "#c8b6b8",
       borderColor = "#272727",
       border = FullB,
-      borderWidth = 1,
+      borderWidth = 0,
       {-
       , position         = Static { xpos = 13, ypos = 1034, width = 1893, height = 32 } Bottom Padded
       , position         = Static { xpos = 0, ypos = 1048, width = 1920, height = 32 } Bottom Flat
       , position         = Static { xpos = 0, ypos = 0, width = 1920, height = 32 } Top Flat
       -}
-      position = Static {xpos = 0, ypos = 0, width = 1920, height = 25},
+      position = Static {xpos = 20, ypos = 15, width = 1880, height = 34},
       alpha = 255,
       overrideRedirect = True,
       lowerOnStart = True,
