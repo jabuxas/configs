652b9822e934242d238619a92ec9dd37a7dd51de
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-01-16 10:08:31 -0300

    add all scripts i really use

---
+++ desktop/configs/scripts/macro.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+running="/tmp/running"
+if [ -f "$running" ]; then
+	pid=$(cat "$running")
+	kill $pid
+	rm "$running"
+else
+	while true; do
+		# spam e
+		# xdotool keydown ctrl ; xdotool keyup ctrl
+		# xdotool keydown e
+		# xdotool keyup e
+
+    # glaive 
+    # xdotool keydown e
+    # sleep 1.1
+    # xdotool keyup e
+    # sleep 0.3
+    # xdotool click 2
+    # sleep 0.2
+    xdotool click 1
+    sleep 1
+
+done &
+echo "$!" >"$running"
+fi
