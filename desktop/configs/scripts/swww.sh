66fdc9a2e284617463262a7b80962d61639dcbce
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-01-16 12:56:51 -0300

    Adapt configs to colorscheme changer script

--- desktop/configs/scripts/swww.sh
+++ desktop/configs/scripts/swww.sh
@@ -5,5 +5,12 @@ SWWW_TRANSITION="grow"
 
 swww-daemon &
 
-sleep 0.5 && swww img -o DP-3 ~/pics/wallpapers/a\ carnival.jpg
-swww img -o HDMI-A-1 ~/pics/wallpapers/the\ true\ old\ god.jpg
+current_theme=$(readlink ~/.config/tmux/theme.conf)
+
+if [[ $current_theme == *tmux-white.conf ]]; then
+    swww img -o DP-3 ~/pics/wallpapers/HOLY.png
+    swww img -o HDMI-A-1 ~/pics/wallpapers/okw\ tf.jpg
+else 
+    swww img -o DP-3 ~/pics/wallpapers/a\ carnival.jpg
+    swww img -o HDMI-A-1 ~/pics/wallpapers/the\ true\ old\ god.jpg
+fi
