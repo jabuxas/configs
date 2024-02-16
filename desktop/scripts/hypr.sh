7be1f838c4cdf6d0ff75498bc2a4c201e2c9eb5c
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-02-16 11:18:19 -0300

    add borders based around colorscheme and matching waybar theme when starting

---
+++ desktop/scripts/hypr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# init script
+
+CURRENT_THEME=$(cat ~/colorscheme)
+
+case "$CURRENT_THEME" in
+    melange) 
+        killall waybar; waybar -s ~/.config/waybar/style-melange.css > /dev/null &
+        hyprctl keyword general:col.active_border "rgba(7892bdff) rgba(6e9b72ff) 45deg"
+    ;;
+    white)
+        killall waybar; waybar -s ~/.config/waybar/style-solarized.css > /dev/null &
+        # hyprctl keyword general:col.active_border  todo
+    ;;
+    red) 
+        killall waybar; waybar > /dev/null &
+        hyprctl keyword general:col.active_border "rgba(ff0038ee) rgba(33e2c5ee) 45deg"
+    ;;
+    *)
+    exit 0
+esac
