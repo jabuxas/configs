46c798a58cac2172de5393b4d34a2f4ef9ab9fd9
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-04-24 23:21:08 -0300

    Update org with capture and org-roam

--- desktop/configs/emacs/custom-file.el
+++ desktop/configs/emacs/custom-file.el
@@ -5,10 +5,18 @@
  ;; If there is more than one, they won't work right.
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(org-modern-mode org-agenda-category-icons org-roam yasnippet-snippets which-key vue-mode projectile powerline-evil paredit magit lsp-ui lsp-pyright helm-xref golint flycheck flx evil-collection doom-themes dap-mode corfu company-go company-fuzzy beacon all-the-icons)))
+   '(helm-rg helm-projectile yasnippet-snippets which-key vue-mode projectile powerline-evil paredit org-super-agenda org-roam org-modern magit lsp-ui lsp-pyright kaolin-themes helm-xref golint flycheck flx evil-collection doom-themes dap-mode corfu company-go company-fuzzy comment-tags beacon all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:inherit default :weight bold :foreground "black" :font "CartographCF Nerd Font" :height 1.7 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "black" :font "CartographCF Nerd Font" :height 1.6))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "black" :font "CartographCF Nerd Font" :height 1.4))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "black" :font "CartographCF Nerd Font" :height 1.3))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "black" :font "CartographCF Nerd Font" :height 1.2))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "black" :font "CartographCF Nerd Font"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "black" :font "CartographCF Nerd Font"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "black" :font "CartographCF Nerd Font"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "black" :font "CartographCF Nerd Font")))))
