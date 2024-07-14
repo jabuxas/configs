8dfa7df5e34a522cee4a58d317d5e46c20174e5b
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-07-14 12:00:21 -0300

    fix: remove unecessary doom emacs config

--- desktop/configs/doom/.config/doom/config.el
+++ desktop/configs/doom/.config/doom/config.el
@@ -75,9 +75,6 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(add-to-list 'custom-theme-load-path "~/docs/everforest-theme")
-(load-theme 'everforest-hard-dark t)
-
 ;; https://superuser.com/questions/271023/can-i-disable-continuation-of-comments-to-the-next-line-in-vim
 (setq +evil-want-o/O-to-continue-comments nil)
 
