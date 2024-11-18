48a08d695d74a69a7c2a2522e57a6a76db3016df
jabuxas <jabuxas@proton.me>
2024-11-18 12:57:57 -0300

    fix: shadows not being enabled on white theme

--- desktop/configs/scripts/scripts/switch.fish
+++ desktop/configs/scripts/scripts/switch.fish
@@ -15,26 +15,26 @@ switch $CURRENT_THEME
         echo changing to white ...
         gsettings set org.gnome.desktop.interface gtk-theme "Windows-95"
         gsettings set org.gnome.desktop.interface icon-theme "nineicons-redux-v0.6"
-        swaymsg shadows enable
         sed -i 's|black.toml|white.toml|' ~/.config/alacritty/alacritty.toml
-        pkill waybar; waybar -s ~/.config/waybar/style-white.css &> /dev/null & disown
-        ~/scripts/swww.sh $wpp1_white $wpp2_white
         ln -sf ~/.config/tmux/themes/tmux-white.conf ~/.config/tmux/theme.conf
         tmux source ~/.config/tmux/tmux.conf
+        pkill waybar; waybar -s ~/.config/waybar/style-white.css &> /dev/null & disown
+        ~/scripts/swww.sh $wpp1_white $wpp2_white
         echo white > ~/colorscheme
+        swaymsg shadows enable
     case white
         echo changing to black...
         gsettings set org.gnome.desktop.interface gtk-theme "Material-Black-Blueberry-LA"
         gsettings set org.gnome.desktop.interface icon-theme "We10X-black-dark"
-        swaymsg shadows disable
         swaymsg client.focused "#ffffff #ffffff #c1c1c1"
         swaymsg client.focused_inactive "#c1c1c1 #212121 #cecece"
         sed -i 's|white.toml|black.toml|' ~/.config/alacritty/alacritty.toml
-        pkill waybar; waybar &> /dev/null & disown
-        ~/scripts/swww.sh $wpp1_solarized $wpp2_solarized
         ln -sf ~/.config/tmux/themes/tmux-monochrome.conf ~/.config/tmux/theme.conf
         tmux source ~/.config/tmux/tmux.conf
+        pkill waybar; waybar &> /dev/null & disown
+        ~/scripts/swww.sh $wpp1_solarized $wpp2_solarized
         echo black > ~/colorscheme
+        swaymsg shadows disable
     case '*'
         echo mmmm what?
 end
