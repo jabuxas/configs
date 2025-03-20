362ad0ac7b4a5b4087b9c0d36af67ea390250b24
jabuxas <jabuxas@proton.me>
2025-03-20 13:42:12 -0300

    feat(work): add better clip

--- work-laptop/configs/doom/.config/doom/config.el
+++ work-laptop/configs/doom/.config/doom/config.el
@@ -23,7 +23,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font "CartographCF Nerd Font-15")
+(setq doom-font "DaddyTimeMono Nerd Font-15")
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
