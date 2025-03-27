d4557877c29e7decc8efb19c6f245e963d67207b
jabuxas <jabuxas@proton.me>
2025-03-27 13:53:13 -0300

    feat: new user new configs

--- shared/scripts/swww.sh
+++ shared/scripts/swww.sh
@@ -9,32 +9,32 @@ CURRENT_THEME=$(cat ~/colorscheme)
 if [[ -z "$1" ]]; then
     case "$CURRENT_THEME" in
         red)
-            FIRST=~/pics/wallpapers/bm5.jpg
-            SECOND=~/pics/wallpapers/bm3.png
+            FIRST=~/images/wallpapers/bm5.jpg
+            SECOND=~/images/wallpapers/bm3.png
             ;;
         rose)
-            SECOND=~/pics/flower.jpg
-            FIRST=~/pics/wallpapers/rose1.jpg
+            SECOND=~/images/flower.jpg
+            FIRST=~/images/wallpapers/rose1.jpg
             ;;
         melange)
-            FIRST=~/pics/wallpapers/melange2.png
-            SECOND=~/pics/wallpapers/melange1.jpg
+            FIRST=~/images/wallpapers/melange2.png
+            SECOND=~/images/wallpapers/melange1.jpg
             ;;
         white)
-            FIRST=~/pics/wallpapers/wh1.png
-            SECOND=~/pics/wallpapers/wh2.jpg
+            FIRST=~/images/wallpapers/wh1.png
+            SECOND=~/images/wallpapers/wh2.jpg
             ;;
         solarized)
-            FIRST=~/pics/wallpapers/sl6.jpg
-            SECOND=~/pics/wallpapers/sl7.jpg
+            FIRST=~/images/wallpapers/sl6.jpg
+            SECOND=~/images/wallpapers/sl7.jpg
             ;;
         monochrome)
-            FIRST=~/pics/wallpapers/monochrome-1.png
-            SECOND=~/pics/wallpapers/monochrome-1.jpg
+            FIRST=~/images/wallpapers/monochrome-1.png
+            SECOND=~/images/wallpapers/monochrome-1.jpg
             ;;
         forest)
-            FIRST=~/pics/wallpapers/ef1.jpg
-            SECOND=~/pics/wallpapers/ef2.jpg
+            FIRST=~/images/wallpapers/ef1.jpg
+            SECOND=~/images/wallpapers/ef2.jpg
     esac
 else
     FIRST=$1
@@ -47,4 +47,4 @@ sleep 0.3
 
 swww img -o HDMI-A-1 $FIRST
 swww img -o DP-1 $SECOND
-# swww img -o DP-3 ~/pics/wallpapers/sl4.jpg
+# swww img -o DP-3 ~/images/wallpapers/sl4.jpg
